6858830c01ef9762e49d4fe78bff0f8da0052844
Mark Nair <marknair@me.com>
2023-11-26 18:09:16 -0600

    Initial Commit

---
+++ SimpleListNavigation/SimpleListNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleListNavigationApp.swift
+//  SimpleListNavigation
+//
+//  Created by Mark Nair on 11/26/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleListNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
